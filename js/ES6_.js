8938873f4bbd5380dc2138369db84d04dce12eed
LiWen <983348993@qq.com>
2019-03-25 17:34:17 +0800

    update
    update

--- js/ES6_.js
+++ js/ES6_.js
@@ -17,14 +17,28 @@ Destructuring:ES6允许按照一定模式，从数组和对象中提取值，对
 	注意：ES6内部使用严格相等运算符（===）判断一个位置是否有值。所以，如果一个数组成员不严格等于undefined，默认值是不会生效的。
 	let和const命令:只要某种数据结构具有Iterator接口，都可以采用数组形式的解构赋值。
 	解构不仅可以用于数组，还可以用于对象：对象的属性没有次序，变量必须与属性同名，才能取到正确的值。
+
 	默认值生效的条件是，对象的属性值严格等于undefined。
  */
 
-new Promise(test).then(function (result) {
-    console.log('成功：' + result);
+
+new Promise(function (resolve, reject) {
+    log('start new Promise...');
+    var timeOut = Math.random() * 2;
+    log('set timeout to: ' + timeOut + ' seconds.');
+    setTimeout(function () {
+        if (timeOut < 1) {
+            log('call resolve()...');
+            resolve('200 OK');
+        }
+        else {
+            log('call reject()...');
+            reject('timeout in ' + timeOut + ' seconds.');
+        }
+    }, timeOut * 1000);
+}).then(function (r) {
+    log('Done: ' + r);
 }).catch(function (reason) {
-    console.log('失败：' + reason);
+    log('Failed: ' + reason);
 });
-function test(){
-	console.log(1);
-}
+
